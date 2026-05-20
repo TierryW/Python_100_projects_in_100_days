1a6759c3a15d00437b7308326aac8a2238d32cd4
TierryW <tierry.souza23@gmail.com>
2026-05-20 11:48:29 -0300

    commit

--- Day_1/webapp.py
+++ Day_1/webapp.py
@@ -3,5 +3,13 @@ import streamlit as st
 st.title("Welcome to the Band name Generator!")
 city = st.text_input("Wich city did you grow up in?\n")
 pet = st.text_input("What is your pet's name? If you don't have one, what name would you give it?\n")
+
 if st.button("Generate Name"):
-    st.success("Your band name could be: " + city + " " + pet)
+    # Remove espaços vazios
+    city = city.strip()
+    pet = pet.strip()
+
+    if city == "" or pet == "":
+        st.error("Please fill in all fields!")
+    else:
+        st.success("Your band name could be: " + city + " " + pet)
