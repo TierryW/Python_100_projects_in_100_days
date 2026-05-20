8500452c343960f619d293e6180a74600343dbd7
TierryW <tierry.souza23@gmail.com>
2026-05-20 10:42:35 -0300

    commit

--- Day_1/app.py
+++ Day_1/app.py
@@ -1,1 +1,4 @@
-print("Hello world!")
+print("Welcome to the Band name Generator!")
+city = input("Wich city did you grow up in?\n")
+pet = input("What is your pet's name? If you don't have one, what name would you give it?\n")
+print("Your band name could be: " + city + " " + pet)
