7396afe902cc5e477e5fb8182d5da9877e78ff71
TierryW <tierry.souza23@gmail.com>
2026-05-20 11:19:57 -0300

    commit

---
+++ Day_1/webapp.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Welcome to the Band name Generator!")
+city = st.text_input("Wich city did you grow up in?\n")
+pet = st.text_input("What is your pet's name? If you don't have one, what name would you give it?\n")
+if st.button("Generate Name"):
+    st.success("Your band name could be: " + city + " " + pet)
