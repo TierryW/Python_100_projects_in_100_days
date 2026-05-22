46f014d9f8f353b7c444371191d9f955286ad764
TierryW <tierry.souza23@gmail.com>
2026-05-22 14:08:44 -0300

    commit

--- Day_3/webapp2.py
+++ Day_3/webapp2.py
@@ -9,96 +9,90 @@ st.title("🏝️ Treasure Island!")
 
 if st.session_state.scene == "start":
     st.markdown("### Your mission is to find the treasure.")
-    st.markdown("### You arrive at a mysterious island during a stormy night.\n")
+    st.markdown("### You arrive at a mysterious island during a stormy night.")
     st.markdown("### Two paths appear in front of you...")
     
     col1, col2 = st.columns(2)
     with col1:
-        if st.button("⬅️ Go Left"):
+        if st.button("⬅️ GO LEFT"):
             st.session_state.scene = "lake"
             st.rerun()
     
     with col2:
-        if st.button("➡️ Go Right"):
+        if st.button("➡️ GO RIGHT"):
             st.session_state.scene = "hole"
             st.rerun()
 
 elif st.session_state.scene == "lake":
-    st.write("""
-            You reach a dark lake with mist floating above the water.
-            
-            There is a small abandoned boat nearby...
-            """)
+    st.markdown("### You reach a dark lake with mist floating above the water.")     
+    st.markdwon("### There is a small abandoned boat nearby...")
     
     col1, col2 = st.columns(2)
     with col1:
-        if st.button("⏳ Wait"):
+        if st.button("⏳ WAIT"):
             st.session_state.scene = "temple"
             st.rerun()
 
     with col2:
-        if st.button("🏊 Swim"):
+        if st.button("🏊 SWIN"):
             st.session_state.scene = 'trout'
             st.rerun()
 
 elif st.session_state.scene == "temple":
-    st.write("""
-            A ghostly boatman silently takes tou to the other side.
-            
-            You arrivve at an ancient temple with 3 glowing doors...
-            """)
+    st.markdown("### A ghostly boatman silently takes tou to the other side.")
+    st.markdown("### You arrivve at an ancient temple with 3 glowing doors...")
 
     col1, col2, col3 = st.columns(3)
     with col1:
-        if st.button("🔴 Red Door"):
+        if st.button("🔴 RED DOOR"):
             st.session_state.scene = "fire"
             st.rerun()
     
     with col2:
-        if st.button("🔵 Blue Door"):
+        if st.button("🔵 BLUE DOOR"):
             st.session_state.scene = "beasts"
             st.rerun()
 
     with col3:
-        if st.button("🟡 Yellow Door"):
+        if st.button("🟡 YELLOW DOOR"):
             st.session_state.scene = "treasure"
             st.rerun()
 
 elif st.session_state.scene == "hole":
-    st.error("You fall into a hidden hole covered with leaves...")
-    st.markdown("### 💀 Game Over")
+    st.markdown("### You fall into a hidden hole covered with leaves...")
+    st.markdown("### 💀 GAME OVER")
 
-    if st.button("🔄 Restart"):
+    if st.button("🔄 RESTART"):
         st.session_state.scene = "start"
         st.rerun()
 
 elif st.session_state.scene == "trout":
-    st.error("Something moves beneath the water...")
-    st.markdown("### 💀 Game Over")
+    st.markdown("### Something moves beneath the water...")
+    st.markdown("### 💀 GAME OVER")
 
-    if st.button("🔄 Restart"):
+    if st.button("🔄 RESTART"):
         st.session_state.scene = "start"
         st.rerun()
 
 elif st.session_state.scene == "fire":
-    st.error("🔥 As soon as you enter, flames burst from t he walls. You are burned alive...")
+    st.markdown("### 🔥 As soon as you enter, flames burst from t he walls. You are burned alive...")
     st.markdown("### 💀 Game Over")
-    if st.button("🔄 Restart"):
+    if st.button("🔄 RESTART"):
         st.session_state.scene = "start"
         st.rerun()
 
 elif st.session_state.scene == "beasts":
-    st.error("🐺 Hungry beasts emerge from the darkness and attack you...")
-    st.markdown("### 💀 Game Over")
-    if st.button("🔄 Restart"):
+    st.markdown("### 🐺 Hungry beasts emerge from the darkness and attack you...")
+    st.markdown("### 💀 GAME OVER")
+    if st.button("🔄 RESTART"):
         st.session_state.scene = "start"
         st.rerun()
 
 elif st.session_state.scene == "treasure":
-    st.success("Inside the yellow room you find mountains of gold and jewels!")
-    st.success("💰 YOU FOUND THE TREASURE!")
+    st.markdown("### Inside the yellow room you find mountains of gold and jewels!")
+    st.markdown("### 💰 YOU FOUND THE TREASURE!")
     st.balloons()
     st.markdown("### 🎉 You Win!")
-    if st.button("🔄 Play Again"):
+    if st.button("🔄 PLAY AGAIN"):
         st.session_state.scene = "start"
         st.rerun()
