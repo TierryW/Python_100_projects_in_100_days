f5198aac69b96c4e1d4da9322edf77ef56de9b83
TierryW <tierry.souza23@gmail.com>
2026-05-25 15:48:57 -0300

    commit

--- Day_4/app.py
+++ Day_4/app.py
@@ -1,89 +1,71 @@
 import random
 
-# Rock
-rock = """
+def rock():
+    # Rock
+    print("""
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-"""
+""")
 
-# Paper
-paper = """
+def paper():
+    # Paper
+    print("""
      _______
 ---'    ____)____
            ______)
           _______)
          _______)
 ---.__________)
-"""
+""")
 
-# Scissors
-scissors = """
+def scissors():
+    # Scissors
+    print("""
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-"""
+""")
 
-print("Welcome to Rock Paper Scissors (RPS)")
-player_choise = int(input("What do you choose?:\n 0 - Rock \n 1 - Paper \n 2 - Scissors \n"))
-computer_choise = random.randint(0, 2)
+def show_choise(choise):
+    if choise == 0:
+        rock()
+    elif choise == 1:
+        paper()
+    elif choise == 2:
+        scissors()
 
-if player_choise == 0:
-    print("You chose:")
-    print(rock)
+def main():
+    print("Welcome to Rock Paper Scissors (RPS)")
+    player = int(input("What do you choose?:\n 0 - Rock \n 1 - Paper \n 2 - Scissors \n"))
+    
+    options = [0, 1, 2]
 
-elif player_choise == 1:
-    print("You chose:")
-    print(paper)
+    if player not in options:
+        print("Error! Choose between 0, 1, or 2.")
+        return
+    
+    computer = random.choice(options)
+    print("\n You chose: ")
+    show_choise(player)
 
-elif player_choise == 2:
-    print("You chose:")
-    print(scissors)
+    print("\n Computer chose: ")
+    show_choise(computer)
 
-else:
-    print("Error! Choose between 0, 1, or 2.")
+    if player == computer:
+        print("It's a draw!")
+    
+    elif ((player == 0 and computer == 2) or (player == 1 and computer == 0)
+        or (player == 2 and computer == 1)):
+        print("You win!")
+    
+    else:
+        print("You lose! Computer wins!")
 
-if computer_choise == 0:
-    print("Computer chose:")
-    print(rock)
-
-elif computer_choise == 1:
-    print("Computer chose:")
-    print(paper)
-
-elif computer_choise == 2:
-    print("Computer chose:")
-    print(scissors)
-
-if player_choise == 0 and computer_choise == 0:
-    print("Draw")
-
-elif player_choise == 0 and computer_choise == 1:
-    print("You Lose")
-
-elif player_choise == 0 and computer_choise == 2:
-    print("You Win")
-
-elif player_choise == 1 and computer_choise == 1:
-    print("Draw")
-
-elif player_choise == 1 and computer_choise == 0:
-    print("You Win")
-
-elif player_choise == 1 and computer_choise == 2:
-    print("You Lose")
-
-elif player_choise == 2 and computer_choise == 2:
-    print("Draw")
-
-elif player_choise == 2 and computer_choise == 1:
-    print("You Win")
-
-elif player_choise == 2 and computer_choise == 0:
-    print("You Lose")
+main()
