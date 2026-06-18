ddd24436f09b9cdf999d4d51b80a3186859e5da2
TierryW <tierry.souza23@gmail.com>
2026-06-18 10:38:30 -0300

    commit

--- Day_008/webapp.py
+++ Day_008/webapp.py
@@ -6,7 +6,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 
 st.set_page_config(page_title="Caesar Cipher", page_icon="📜")
 
-st.title("CAESAR CIPHER")
+st.title("WELCOME TO THE:")
 st.code(logo_ceasar_cipher.logo)
 
 def ceasar(original_text, shift_amount, encode_or_decode):
@@ -26,26 +26,17 @@ def ceasar(original_text, shift_amount, encode_or_decode):
 
     return cipher_text
 
-# Escolha da operação
-direction = st.radio(
-    "CHOOSE AN OPTION:",
-    ["ENCODE", "DECODE"],
-    horizontal=True
-)
-
+direction = st.radio("CHOOSE AN OPTION:", ["ENCODE", "DECODE"],horizontal=True)
 text = st.text_input("TYPE YOUR MESSAGE:")
-
-shift = st.number_input(
-    "TYPE THE SHIFT NUMBER:",
-    min_value=1,
-    step=1
-)
+shift = st.number_input("TYPE THE SHIFT NUMBER:",min_value=1,step=1)
 
 if st.button("RUN CIPHER"):
-    result = ceasar(text.lower(), shift, direction)
-
-    st.success(f"HERE IS THE {direction}D RESULT:")
-    st.code(result)
+    if text.strip() == "":
+        st.warning("PLEASE ENTER A MESSAGE!")
+    else:
+        result = ceasar(text.lower(), shift, st.session_state.scene)
+        st.success(f"HERE IS THE {direction}D MESSAGE:")
+        st.code(result)
 
 if st.button("RESET"):
     st.rerun()
