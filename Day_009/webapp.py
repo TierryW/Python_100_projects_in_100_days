1433518aa852aefbece937db5c78966a27f90778
TierryW <tierry.souza23@gmail.com>
2026-07-02 10:53:47 -0300

    commit

--- Day_009/webapp.py
+++ Day_009/webapp.py
@@ -1,28 +1,39 @@
 import streamlit as st
-import json
-import os
 import art
 
-st.title("Secret Auction")
-st.text(art.logo)
+st.set_page_config(page_title="Secret Auction Program", page_icon="💲")
+st.title("Secret Auction Program")
+st.code(art.logo)
+
+if "bids" not in st.session_state:
+    st.session_state.bids = {}
 
 name = st.text_input("What is your name?")
-bid = st.number_input("What is your bid?", min_value=0.0, step=1.0)
+price = st.number_input("What is your bid? $", min_value=0.0, step=1.0)
 
-if st.button("Submit Bid"):
-    if name:
-        if os.path.exists("bids.json"):
-            with open("bids.json", "r") as f:
-                bids = json.load(f)
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("ADD BID"):
+        if name:
+            st.session_state.bids[name] = price
+            st.success(f"Bid added for {name}!")
         else:
-            bids = {}
+            st.error("Please enter your name.")
 
-        bids[name] = bid
+with col2:
+    if st.button("FINISH AUCTION"):
+        if st.session_state.bids:
+            winner = max(st.session_state.bids, key=st.session_state.bids.get)
+            highest_bid = st.session_state.bids[winner]
 
-        with open("bids.json", "w") as f:
-            json.dump(bids, f)
+            st.success(
+                f"🏆 The winner is **{winner}** with a bid of **${highest_bid:.2f}**"
+            )
 
-        st.success("Your bid has been submitted!")
-        st.info("Please pass the computer to the next bidder.")
-    else:
-        st.error("Enter your name.")
+            st.subheader("All Bids")
+            st.write(st.session_state.bids)
+        else:
+            st.warning("No bids have been entered.")
+if st.button("RESET"):
+    st.rerun()
